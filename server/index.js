3464c06d85e595292791aa40fe5c874774a7ac98
phaANTom2303 <anikavy.goenka@gmail.com>
2025-10-21 22:41:36 +0530

    created basic environment variable system

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const { mongoose } = require('mongoose');
 const { displayLeetcodeUser } = require('./leetcode.js');
+const cors = require('cors');
+const submissionRouter = require('./routes/submissionRoutes.js');
+
+mongoose.connect("mongodb://127.0.0.1:27017/lc-autorevise")
+    .then(console.log("mongoDB Connected"))
+    .catch((err) => console.log(err));
 // Middleware
+app.use(cors({
+    
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,7 +22,7 @@ app.get('/', (req, res) => {
     res.json({ message: 'LC AutoRevise Server is running!' });
 });
 
-
+app.use('/submissions', submissionRouter);
 
 // Start server
 app.listen(PORT, () => {
--- server/leetcode.js
+++ server/leetcode.js
@@ -7,7 +7,7 @@ async function displayLeetcodeUser(username) {
         const user = await leetcode.user(username);
         console.log(user);
         const requser = user.matchedUser.profile;
-        console.log(`Required User = ${JSON.stringify(requser, null, 2)}`);
+        // console.log(`Required User = ${JSON.stringify(requser, null, 2)}`);
         // console.log(`Required User = ${requser}`);
     } catch (error) {
         console.error(`Error fetching user ${username}:`, error);
